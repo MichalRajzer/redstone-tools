457fa933e6e7d749ff3a2f207e874c493be31480
Polish  Fuze <michal.rajzer03@gmail.com>
2022-02-22 03:18:47 +0100

    fixed y-z mixup, changed the way directions work

--- main.py
+++ main.py
@@ -12,70 +12,103 @@ class RedstoneHelper():
         self.debug = debug
         self.framerate = framerate
 
-    def xLine(self, length, xOffset=0, zOffset=0, yOffset=0, block="white_concrete", dir=0):
+    def Line(self, length, xOffset=0, yOffset=0, zOffset=0, block="white_concrete", dir=0):
         if dir == 0:
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset} ~{zOffset-1} ~{yOffset} ~{length+xOffset} ~{zOffset-1} ~{yOffset} minecraft:{block}\n")
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset-1} ~{zOffset} ~{length+xOffset} ~{yOffset-1} ~{zOffset} minecraft:{block}\n")
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset} ~{zOffset} ~{yOffset} ~{length+xOffset} ~{zOffset} ~{yOffset} minecraft:redstone_wire\n")
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset} ~{zOffset} ~{length+xOffset} ~{yOffset} ~{zOffset} minecraft:redstone_wire\n")
+        if dir == 1:
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset-1} ~{zOffset} ~{xOffset} ~{yOffset-1} ~{length+zOffset} minecraft:{block}\n")
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset} ~{zOffset} ~{xOffset} ~{yOffset} ~{length+zOffset} minecraft:redstone_wire\n")
         if dir == 2:
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset} ~{zOffset-1} ~{yOffset} ~{-length+xOffset} ~{zOffset-1} ~{yOffset} minecraft:{block}\n")
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset-1} ~{zOffset} ~{-length+xOffset} ~{yOffset-1} ~{zOffset} minecraft:{block}\n")
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset} ~{zOffset} ~{yOffset} ~{-length+xOffset} ~{zOffset} ~{yOffset} minecraft:redstone_wire\n")
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset} ~{zOffset} ~{-length+xOffset} ~{yOffset} ~{zOffset} minecraft:redstone_wire\n")
+        if dir == 3:
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset-1} ~{zOffset} ~{xOffset} ~{yOffset-1} ~{-length+zOffset} minecraft:{block}\n")
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset} ~{zOffset} ~{xOffset} ~{yOffset} ~{-length+zOffset} minecraft:redstone_wire\n")
 
-    def xBus(self, length, width=8, xOffset=-1, zOffset=0, yOffset=0, gap=1):
+    def Bus(self, length, width=8, xOffset=-1, yOffset=0, zOffset=0, gap=1, dir=0):
         for j in range(width):
             for i in range(int(length/18)):
-                self.xLine(14, xOffset=2+18*i+xOffset, zOffset=zOffset, yOffset=yOffset+j*(gap+1))
-                self.instantRepeater(xOffset=16+18*i+xOffset, zOffset=1+zOffset, yOffset=yOffset+j*(gap+1))
+                self.Line(14, xOffset=2+18*i+xOffset, yOffset=yOffset, zOffset=zOffset+j*(gap+1))
+                self.instantRepeater(xOffset=16+18*i+xOffset, yOffset=1+yOffset, zOffset=zOffset+j*(gap+1))
                 if self.debug:
                     print(f"Line {j}, stage {i}")
-            self.xLine(length%18-1, xOffset=2+18*int(length/18)+xOffset, zOffset=zOffset, yOffset=yOffset+j*(gap+1))
+            self.Line(length%18-1, xOffset=2+18*int(length/18)+xOffset, yOffset=yOffset, zOffset=zOffset+j*(gap+1))
 
 
 
-    def instantRepeater(self, xOffset=0, zOffset=0, yOffset=0, dir=0): 
+    def instantRepeater(self, xOffset=0, yOffset=0, zOffset=0, dir=0): 
         if dir==0:
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset} ~{zOffset-1} ~{yOffset} ~{xOffset} ~{zOffset-1} ~{yOffset} minecraft:target\n")
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset-1} ~{zOffset} ~{xOffset} ~{yOffset-1} ~{zOffset} minecraft:target\n")
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset} ~{zOffset} ~{yOffset} ~{xOffset} ~{zOffset} ~{yOffset} minecraft:sticky_piston[facing=east]\n")
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset} ~{zOffset} ~{xOffset} ~{yOffset} ~{zOffset} minecraft:sticky_piston[facing=east]\n")
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset+2} ~{zOffset-1} ~{yOffset} ~{xOffset+2} ~{zOffset-1} ~{yOffset} minecraft:sticky_piston[facing=west]\n")
+            self.keyboard.type(f"fill ~{xOffset+2} ~{yOffset-1} ~{zOffset} ~{xOffset+2} ~{yOffset-1} ~{zOffset} minecraft:sticky_piston[facing=west]\n")
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset+1} ~{zOffset} ~{yOffset} ~{xOffset+1} ~{zOffset} ~{yOffset} minecraft:redstone_block\n")
+            self.keyboard.type(f"fill ~{xOffset+1} ~{yOffset} ~{zOffset} ~{xOffset+1} ~{yOffset} ~{zOffset} minecraft:redstone_block\n")
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset+3} ~{zOffset-1} ~{yOffset} ~{xOffset+3} ~{zOffset-1} ~{yOffset} minecraft:white_concrete\n")
+            self.keyboard.type(f"fill ~{xOffset+3} ~{yOffset-1} ~{zOffset} ~{xOffset+3} ~{yOffset-1} ~{zOffset} minecraft:white_concrete\n")
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset+3} ~{zOffset} ~{yOffset} ~{xOffset+3} ~{zOffset} ~{yOffset} minecraft:redstone_wire\n")
+            self.keyboard.type(f"fill ~{xOffset+3} ~{yOffset} ~{zOffset} ~{xOffset+3} ~{yOffset} ~{zOffset} minecraft:redstone_wire\n")
         elif dir == 1:
-            pass
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset-1} ~{zOffset} ~{xOffset} ~{yOffset-1} ~{zOffset} minecraft:target\n")
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset} ~{zOffset} ~{xOffset} ~{yOffset} ~{zOffset} minecraft:sticky_piston[facing=south]\n")
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset-1} ~{zOffset+2} ~{xOffset} ~{yOffset-1} ~{zOffset+2} minecraft:sticky_piston[facing=north]\n")
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset} ~{zOffset+1} ~{xOffset} ~{yOffset} ~{zOffset+1} minecraft:redstone_block\n")
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset-1} ~{zOffset+3} ~{xOffset} ~{yOffset-1} ~{zOffset+3} minecraft:white_concrete\n")
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset} ~{zOffset+3} ~{xOffset} ~{yOffset} ~{zOffset+3} minecraft:redstone_wire\n")
         elif dir == 2:
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset} ~{zOffset-1} ~{yOffset} ~{xOffset} ~{zOffset-1} ~{yOffset} minecraft:target\n")
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset-1} ~{zOffset} ~{xOffset} ~{yOffset-1} ~{zOffset} minecraft:target\n")
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset} ~{zOffset} ~{yOffset} ~{xOffset} ~{zOffset} ~{yOffset} minecraft:sticky_piston[facing=west]\n")
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset} ~{zOffset} ~{xOffset} ~{yOffset} ~{zOffset} minecraft:sticky_piston[facing=west]\n")
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset-2} ~{zOffset-1} ~{yOffset} ~{xOffset-2} ~{zOffset-1} ~{yOffset} minecraft:sticky_piston[facing=east]\n")
+            self.keyboard.type(f"fill ~{xOffset-2} ~{yOffset-1} ~{zOffset} ~{xOffset-2} ~{yOffset-1} ~{zOffset} minecraft:sticky_piston[facing=east]\n")
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset-1} ~{zOffset} ~{yOffset} ~{xOffset-1} ~{zOffset} ~{yOffset} minecraft:redstone_block\n")
+            self.keyboard.type(f"fill ~{xOffset-1} ~{yOffset} ~{zOffset} ~{xOffset-1} ~{yOffset} ~{zOffset} minecraft:redstone_block\n")
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset-3} ~{zOffset-1} ~{yOffset} ~{xOffset-3} ~{zOffset-1} ~{yOffset} minecraft:white_concrete\n")
+            self.keyboard.type(f"fill ~{xOffset-3} ~{yOffset-1} ~{zOffset} ~{xOffset-3} ~{yOffset-1} ~{zOffset} minecraft:white_concrete\n")
             self.OpenLine()
-            self.keyboard.type(f"fill ~{xOffset-3} ~{zOffset} ~{yOffset} ~{xOffset-3} ~{zOffset} ~{yOffset} minecraft:redstone_wire\n")
+            self.keyboard.type(f"fill ~{xOffset-3} ~{yOffset} ~{zOffset} ~{xOffset-3} ~{yOffset} ~{zOffset} minecraft:redstone_wire\n")
         elif dir == 3:
-            pass
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset-1} ~{zOffset} ~{xOffset} ~{yOffset-1} ~{zOffset} minecraft:target\n")
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset} ~{zOffset} ~{xOffset} ~{yOffset} ~{zOffset} minecraft:sticky_piston[facing=north]\n")
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset-1} ~{zOffset-2} ~{xOffset} ~{yOffset-1} ~{zOffset-2} minecraft:sticky_piston[facing=south]\n")
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset} ~{zOffset-1} ~{xOffset} ~{yOffset} ~{zOffset-1} minecraft:redstone_block\n")
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset-1} ~{zOffset-3} ~{xOffset} ~{yOffset-1} ~{zOffset-3} minecraft:white_concrete\n")
+            self.OpenLine()
+            self.keyboard.type(f"fill ~{xOffset} ~{yOffset} ~{zOffset-3} ~{xOffset} ~{yOffset} ~{zOffset-3} minecraft:redstone_wire\n")
 
     def tester(self):
-        self.xBus(length=64, width=4, gap=2)
+        # self.Bus(length=64, width=4, gap=2)
+        self.instantRepeater(zOffset=2, dir=3)
  
     def OpenLine(self, key="/"):
         self.keyboard.press(key)
         self.keyboard.release(key)
         time.sleep(1.0/self.framerate)
 
-for i in range(5):
+for i in range(3):
     print(f"{5-i}!")
     time.sleep(1)
 print("Start!")
